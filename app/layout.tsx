75e9fe93ef038aa96658b9e62d2a5d53a129bfac
beargame <hoyoung7827@gmail.com>
2025-09-03 17:45:25 +0900

    모바일 환경일때, 줌이나 드래그 못하게 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,6 +85,7 @@ export default async function RootLayout({
   return (
     <html lang={lang} suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
